e4ea357eeb3a59474d07955e95fd8669ef3e6104
jlnvi <157695184+jlnvi@users.noreply.github.com>
2025-01-08 17:23:43 +0100

    Commit

--- famille.pl
+++ famille.pl
@@ -6,19 +6,23 @@ homme(paul).
 homme(jacques).
 femme(marie).
 femme(sophie).
+femme(julie).
 
-
+parent(julie, sophie).
 parent(pierre, paul).
 parent(marie, paul).
 parent(marc, sophie).
 parent(jacques, marc).
+
 
 % Définir des règles
 
 pere(X, Y) :- homme(X), parent(X, Y).
 mere(X, Y) :- femme(X), parent(X, Y).
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
-freresoeur(Y, Z) :- parent(X, Y), parent(X, Z), Y \= Z.
+freresoeur(Y, Z) :- parent(X, Y), parent(X, Z), not(Y=Z).
+oncletante(X, Y) :- freresoeur(X, Z), parent(Z, Y).
+cousin(X, Y) :- parent(Z, X), freresoeur(Z, W), parent(W, Y).
 
 % Règles de longueur
 
@@ -27,4 +31,6 @@ longueur([_ | Queue], N) :- longueur(Queue, M), N is M + 1.
 
 present([N|_], N).
 present([_ | Queue], X) :- present(Queue, X).
+
+
 
